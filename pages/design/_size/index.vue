28b33b3f5940b708232d9c3c0b67fbcf5991e59c
Vladislav Manakov <footzi@mail.ru>
2018-11-28 22:06:06 +0300

    Добавляет статичные картинки для проверки дизайна

---
+++ pages/design/_size/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img :src="`../images/design/${size}.jpg`">
+    </div>
+</template>
+
+<style lang="scss">
+    img {
+            width: 100%;
+        }
+</style>
+
+<script>
+export default {
+    computed: {
+        size() {
+            return this.$route.params.size;
+        }
+    }
+}
+</script>
---
+++ pages/design/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Design page</h1>
+        <nuxt-link to="/design/320">320</nuxt-link>
+        <nuxt-link to="/design/670">670</nuxt-link>
+        <nuxt-link to="/design/960">960</nuxt-link>
+        <nuxt-link to="/design/1920">1920</nuxt-link>
+    </div>
+</template>
