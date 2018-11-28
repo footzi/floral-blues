fca3fe1fa890bfac41563a7d039f33e726a880d7
Vladislav Manakov <footzi@mail.ru>
2018-11-28 21:19:49 +0300

    Правит запуск npm start

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,10 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  server: {
+    port: process.env.port || 3000, // default: 3000
+    host: process.env.host || '0.0.0.0', // default: localhost
+  },
 }
 
