dbc6b5daaae88d89eab30e9986bf22f85cf7b462
Vladislav Manakov <footzi@mail.ru>
2018-11-29 18:17:20 +0300

    Пробует добавить axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL:  environment.API_URL || 'http://localhost:3000'
+  baseURL: process.env.BASE_URL || 'http://localhost:3000'
 })
